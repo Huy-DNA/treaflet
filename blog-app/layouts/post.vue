803a277627133052374749c6d03bc529e5015fd5
HuyDNA <huydo862003@gmail.com>
2023-03-31 23:20:37 +0700

    Add to-top button to all post pages
    For better UX.
    
    - Add a TopButton component.
    - Add a post layout for all posts.
    - Add .output to .gitignore

---
+++ blog-app/layouts/post.vue
@@ -0,0 +1,44 @@
+<template>
+    <TopButton v-if='shouldShowTopButton' class='top-button'/>
+    <slot/>
+</template>
+
+<script setup lang='ts'>
+    const shouldShowTopButton = ref<boolean>(false);
+
+    const handleScroll = () => {
+        if (window.scrollY > 100)
+            shouldShowTopButton.value = true;
+        else
+            shouldShowTopButton.value = false;
+    }
+
+    onMounted(() => {
+        window.addEventListener("scroll", handleScroll);
+    })
+
+    onUnmounted(() => {
+        window.removeEventListener("scroll", handleScroll);
+    })
+
+</script>
+
+<style lang='scss' scoped>
+    .top-button {
+        position: fixed;
+        z-index: 1;
+    }
+
+    .top-button {
+        right: 6.5vw;
+        bottom: 6.5vw;
+    }
+
+    @media(min-width: 700px) {
+        .top-button {
+            right: 3vw;
+            bottom: 3vw;
+        }
+    }
+</style>
+
---
+++ blog-app/pages/posts/post.vue
@@ -0,0 +1,5 @@
+<template>
+    <NuxtLayout name='post'>
+        <NuxtPage/>
+    </NuxtLayout>
+</template>
