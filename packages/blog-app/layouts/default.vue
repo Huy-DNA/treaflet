c4a88b8f932478df7ed6de54435527ed99d19873
Huy-DNA <huydo862003@gmail.com>
2024-08-29 23:13:19 +0700

    feat: basic default layout

---
+++ packages/blog-app/layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nav class='header'>
+    </nav>
+    <section class='content'>
+      <slot />
+    </section>
+    <footer class='footer'>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+  .header {
+  }
+  .content {
+  }
+  .footer {
+  }
+</style>
