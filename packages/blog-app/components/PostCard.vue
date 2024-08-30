716a65ca2e5075b6717f0cddb59d6aa9822a1f2e
Huy-DNA <huydo862003@gmail.com>
2024-08-30 22:37:20 +0700

    fix: invert daysDiff comparison

--- packages/blog-app/components/PostCard.vue
+++ packages/blog-app/components/PostCard.vue
@@ -50,7 +50,7 @@
   const createdAt = computed(() => {
     const now = DateTime.now();
     const daysDiff = Math.floor(now.diff(props.post.createdAt, 'days').toObject().days!);
-    if (daysDiff > 7) {
+    if (daysDiff <= 7) {
       return `${daysDiff} days ago`;
     }
     return props.post.createdAt.toFormat('DDD');
