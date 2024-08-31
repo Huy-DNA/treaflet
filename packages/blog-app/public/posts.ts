0eafb038d411383bd99ba1e0f1cb458dc3c9b8b7
Huy-DNA <huydo862003@gmail.com>
2024-08-31 10:25:30 +0700

    test: add mock data

--- packages/blog-app/public/posts.ts
+++ packages/blog-app/public/posts.ts
@@ -7,6 +7,15 @@ const lastMonth = DateTime.now().minus(Duration.fromObject({ months: 1 }));
 /* Mock data for testing - real data is generated during build */
 const posts: PostCollection = [
   {
+    title: 'A very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very long title',
+    slug: 'a-very-long-post',
+    createdAt: today,
+    modifiedAt: today,
+    summary: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32',
+    tags: [ { name: 'C++' }, { name: 'glibc' } ],
+    thumbnailUrl: '/logos.jpeg',
+  },
+  {
     title: 'Today post',
     slug: 'today-post',
     createdAt: today,
