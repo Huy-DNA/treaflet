428ffc4aeeaea8ddaebd2dbf58fe2b8e8313f6dc
Huy-DNA <huydo862003@gmail.com>
2024-11-25 13:58:59 +0700

    fix: migrate to eslint.cjs

---
+++ eslint.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    '@nuxtjs/eslint-config-typescript'
+  ]
+};
